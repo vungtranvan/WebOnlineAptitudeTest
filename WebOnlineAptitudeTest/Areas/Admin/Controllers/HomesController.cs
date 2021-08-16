ffb49ea65137f684e150c67336e3014803642f4e
vungtranvan <vungka321@gmail.com>
2021-08-16 09:39:57 +0700

    Add project files.

---
+++ WebOnlineAptitudeTest/Areas/Admin/Controllers/HomesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebOnlineAptitudeTest.Areas.Admin.Controllers
+{
+    public class HomesController : Controller
+    {
+        // GET: Admin/Homes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
