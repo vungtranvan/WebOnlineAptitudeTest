5ffc4dd780ddfb582f47182da5e56b07d33cae8e
Dũng Hoàng <hoangtrungdungxz@gmail.com>
2021-08-21 21:30:12 +0700

    21/8

--- WebOnlineAptitudeTest/App_Start/BundleConfig.cs
+++ WebOnlineAptitudeTest/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace WebOnlineAptitudeTest
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/site").Include(
+                    "~/Content/Site.css"));
         }
     }
 }
