2ead2596cf4299ba56cf9e917db5c35d69302871
Hieu Nguyen <henrykappak@gmail.com>
2021-08-28 21:32:21 +0700

    update all

--- WebOnlineAptitudeTest/Areas/Admin/Controllers/AccountController.cs
+++ WebOnlineAptitudeTest/Areas/Admin/Controllers/AccountController.cs
@@ -10,6 +10,7 @@ namespace WebOnlineAptitudeTest.Areas.Admin.Controllers
 {
     public class AccountController : BaseController
     {
+        // chuc nang search đau
         private readonly IAdminRepository _adminRepository;
         public AccountController(IAdminRepository adminRepository)
         {
@@ -18,7 +19,20 @@ namespace WebOnlineAptitudeTest.Areas.Admin.Controllers
         
         public ActionResult Index()
         {
+            ViewBag.ListpageSize = new List<int>() { 5, 10, 15, 20, 50, 100 };
             return View();
+        }
+
+        public JsonResult LoadData(string keyword, int page, int pageSize)
+        {
+            var result = _adminRepository.GetData(keyword, page, pageSize);
+
+            return Json(new
+            {
+                data = result.Items,
+                totalRow = result.TotalRow,
+                status = true
+            }, JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
@@ -40,6 +54,147 @@ namespace WebOnlineAptitudeTest.Areas.Admin.Controllers
                 status = result,
                 title
             });
+        }
+
+        public JsonResult Details(int id)
+        {
+            var acc = _adminRepository.GetSingleById(id);
+
+            if (acc == null)
+            {
+                return Json(new
+                {
+                    data = acc.CreatedDate.ToString(),
+                    createdDate =  acc.CreatedDate.ToString(),
+                    updatedDate =  acc.UpdatedDate.ToString(),
+                    status = false
+                }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new
+            {
+                data = acc,
+                createdDate = acc.CreatedDate.ToString(),
+                updatedDate = acc.UpdatedDate.ToString(),
+                status = true
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult InsertOrUpdate(int? id)
+        {
+            if (id != null)
+            {
+                var acc = _adminRepository.GetSingleById(id.Value);
+                acc.Password = "";
+                return View(acc);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult InsertOrUpdate(Models.Entities.Admin acc)
+        {
+            acc.UpdatedDate = DateTime.Now;
+            var idAcc = acc.Id;
+
+            #region Validate data input
+            var checkEmail = _adminRepository.CheckContains(c => c.Email.Equals(acc.Email));
+            var checkUserName = _adminRepository.CheckContains(c => c.UserName.Equals(acc.UserName));
+
+            if (idAcc == 0)
+            {
+                if (string.IsNullOrEmpty(acc.Password))
+                    ModelState.AddModelError("Password", "This field is required");
+
+                if (checkEmail)
+                    ModelState.AddModelError("Email", "Email already exists !!!");
+                if (checkUserName)
+                    ModelState.AddModelError("UserName", "UserName already exists !!!");
+            }
+            else
+            {
+                var account = _adminRepository.GetSingleById(idAcc);
+                if (account == null)
+                    return HttpNotFound();
+
+                if (checkEmail && !account.Email.Equals(acc.Email))
+                    ModelState.AddModelError("Email", "Email already exists !!!");
+                if (checkUserName && !account.UserName.Equals(acc.UserName))
+                    ModelState.AddModelError("UserName", "UserName already exists !!!");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(acc);
+            }
+            #endregion
+
+            #region Processing Image
+            if (string.IsNullOrEmpty(acc.Image))
+            {
+                acc.Image = "/Content/default-avatar.jpg";
+            }
+            else
+            {
+                string hostUrl = Request.Url.Scheme + "://" + Request.Url.Host;
+                if (!acc.Image.Contains(hostUrl))
+                    acc.Image = hostUrl + acc.Image;
+                acc.Image = acc.Image.CutHostAndSchemePathFile();
+            }
+            #endregion
+
+            var result = _adminRepository.InsertOrUpdate(acc);
+
+            if (result == true)
+            {
+                if (idAcc == 0)
+                {
+                    TempData["XMessage"] = new XMessage("Notification", "Add Successfull !!!", EnumCategoryMess.success);
+                }
+                else
+                {
+                    TempData["XMessage"] = new XMessage("Notification", "Edit Successfull !!!", EnumCategoryMess.success);
+                }
+            }
+            else
+            {
+                if (idAcc == 0)
+                {
+                    TempData["XMessage"] = new XMessage("Notification", "Add Error !!!", EnumCategoryMess.error);
+                }
+                else
+                {
+                    TempData["XMessage"] = new XMessage("Notification", "Edit Error !!!", EnumCategoryMess.error);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public JsonResult Locked(int id)
+        {
+            var title = "Notification";
+            var account = _adminRepository.Locked(id);
+
+            if (!account)
+            {
+                return Json(new
+                {
+                    message = "Delete Error !!!",
+                    status = false,
+                    title
+                }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new
+            {
+                message = "Delete Successfull !!!",
+                status = true,
+                title
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
--- WebOnlineAptitudeTest/Models/Repositories/AdminRepository.cs
+++ WebOnlineAptitudeTest/Models/Repositories/AdminRepository.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebOnlineAptitudeTest.Areas.Admin.Data.Model.Pagings;
 using WebOnlineAptitudeTest.Models.Entities;
 using WebOnlineAptitudeTest.Models.Infrastructure;
 
@@ -10,6 +11,9 @@ namespace WebOnlineAptitudeTest.Models.Repositories
     public interface IAdminRepository : IRepository<Admin>
     {
         bool ChangePass(string userName, string passOld, string passNew);
+        bool InsertOrUpdate(Admin acc);
+        bool Locked(int id);
+        PagingModel<Admin> GetData(string keyword, int page, int pageSize);
     }
 
     public class AdminRepository : RepositoryBase<Admin>, IAdminRepository
@@ -34,5 +38,72 @@ namespace WebOnlineAptitudeTest.Models.Repositories
             _unitOfWork.Commit();
             return true;
         }
+
+        public bool InsertOrUpdate(Models.Entities.Admin acc)
+        {
+            if (acc.Id == 0)
+            {
+                acc.Password = acc.Password.ToMD5();
+                acc.CreatedDate = DateTime.Now;
+                acc.Deleted = false;
+                base.Add(acc);
+            }
+            else
+            {
+                var account = base.GetSingleById(acc.Id);
+                if (account == null)
+                {
+                    return false;
+                }
+                account.UpdatedDate = DateTime.Now;
+                account.UserName = acc.UserName;
+                account.Email = acc.Email;
+                account.Sex = acc.Sex;
+                account.Image = acc.Image;
+                if (acc.Password != null)
+                {
+                    account.Password = acc.Password.ToMD5();
+                }
+                base.Update(account);
+            }
+            _unitOfWork.Commit();
+            return true;
+        }
+
+        public PagingModel<Admin> GetData(string keyword, int page, int pageSize)
+        {
+            List<Admin> lstAcc = new List<Admin>();
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                lstAcc = base.Get(
+                    filter: c => c.Deleted == false && (c.UserName.ToLower().Contains(keyword.ToLower())
+                    || c.DisplayName.ToLower().Contains(keyword.ToLower()) || c.Email.ToLower().Contains(keyword.ToLower())),
+                    orderBy: c => c.OrderByDescending(x => x.Id)).ToList();
+            }
+            else
+            {
+                lstAcc = base.Get(filter: c => c.Deleted == false, orderBy: c => c.OrderByDescending(x => x.Id)).ToList();
+            }
+
+            int totalRow = lstAcc.Count();
+
+            var data = lstAcc.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+            return new PagingModel<Admin>() { TotalRow = totalRow, Items = data };
+        }
+
+        public bool Locked(int id)
+        {
+            var acc = base.GetSingleById(id);
+            if (acc == null)
+            {
+                return false;
+            }
+            acc.Deleted = !acc.Deleted;
+            _unitOfWork.Commit();
+            return true;
+        }
+
     }
 }
