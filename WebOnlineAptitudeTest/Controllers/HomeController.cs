0a17623d9b19e7d04fe395956eb7c819308c7858
Dũng Hoàng <hoangtrungdungxz@gmail.com>
2021-08-31 09:41:51 +0700

    aaa

--- WebOnlineAptitudeTest/Controllers/HomeController.cs
+++ WebOnlineAptitudeTest/Controllers/HomeController.cs
@@ -5,34 +5,50 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebOnlineAptitudeTest.Models.Entities;
+using WebOnlineAptitudeTest.Models.Infrastructure;
+using WebOnlineAptitudeTest.Models.Repositories;
+using WebOnlineAptitudeTest.Views.ViewModel;
 
 namespace WebOnlineAptitudeTest.Controllers
 {
     public class HomeController : BaseFrController
     {
-        public HomeController()
+        private readonly IQuestionRepository _questionRepository;
+        private readonly ICategoryExamRepository _categoryExamRepository;
+        private readonly IAnswerRepository _answerRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+
+        public HomeController(IQuestionRepository questionRepository,
+            ICategoryExamRepository categoryExamRepository,
+            IAnswerRepository answerRepository,
+            IUnitOfWork unitOfWork)
         {
+            _questionRepository = questionRepository;
+            _categoryExamRepository = categoryExamRepository;
+            _answerRepository = answerRepository;
+            _unitOfWork = unitOfWork;
+
         }
         public ActionResult Index()
         {
             return View();
         }
 
-        [HttpPost]
-        public ActionResult AptidudeTest(FormCollection testRequest)
+        public ActionResult AptidudeTest(ResultViewModel resultViewModel)
         {
 
-            string reQuestQuest = "";
+            //string reQuestQuest = "";
 
-            var collection  =  testRequest.Get("collection");
+            //var collection  =  testRequest.Get("collection");
 
-            foreach (var key in testRequest.AllKeys)
-            {
-                if (key.Contains("collection"))
-                {
-                    reQuestQuest = testRequest.Get(key);
-                }
-            }
+            //foreach (var key in testRequest.AllKeys)
+            //{
+            //    if (key.Contains("collection"))
+            //    {
+            //        reQuestQuest = testRequest.Get(key);
+            //    }
+            //}
             //var abc = HttpUtility.ParseQueryString(collection);
 
 
@@ -47,6 +63,20 @@ namespace WebOnlineAptitudeTest.Controllers
 
 
             return Json(new { Response, Status = "Success", PartName = "123" }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult CreateQuest()
+        {
+            var result = _questionRepository.GetQuestion(1);
+
+
+            var data = JsonConvert.SerializeObject(result, Formatting.Indented,
+               new JsonSerializerSettings
+               {
+                   ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+               });
+
+            return Json(new { data, Status = "Success", PartId = 1}, JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ WebOnlineAptitudeTest/Views/ViewModel/ResultViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebOnlineAptitudeTest.Views.ViewModel
+{
+    public class ResultViewModel
+    {
+        public int CandidateId { get; set; }
+        public List<ResultQuest> ResultQuest { get; set; }
+
+    }
+
+    public class ResultQuest
+    {
+        public int QuestionId { get; set; }
+
+        public string Answers { get; set; }
+
+    }
+
+
+}
