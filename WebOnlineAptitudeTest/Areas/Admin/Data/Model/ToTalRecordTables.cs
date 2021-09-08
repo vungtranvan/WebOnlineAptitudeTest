b4b610dac2052b557505556263338ef9239a33db
vungtranvan <vungka321@gmail.com>
2021-09-08 17:38:01 +0700

    add chart candidate status

---
+++ WebOnlineAptitudeTest/Areas/Admin/Data/Model/ToTalRecordTables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebOnlineAptitudeTest.Areas.Admin.Data.Model
+{
+    public class ToTalRecordTables
+    {
+        public int TotalCandidate { get; set; }
+        public int TotalTestSchedule { get; set; }
+        public int TotalAdmin { get; set; }
+        public int TotalQuestion { get; set; }
+    }
+}
