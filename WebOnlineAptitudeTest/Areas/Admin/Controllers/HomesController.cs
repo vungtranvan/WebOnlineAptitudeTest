7de5262c9a4c0dc7f43330b389437fca8d84d6b4
Dũng Hoàng <hoangtrungdungxz@gmail.com>
2021-09-12 15:06:22 +0700

    count

--- WebOnlineAptitudeTest/Areas/Admin/Controllers/HomesController.cs
+++ WebOnlineAptitudeTest/Areas/Admin/Controllers/HomesController.cs
@@ -9,14 +9,21 @@ using WebOnlineAptitudeTest.Areas.Admin.Data.Model;
 using WebOnlineAptitudeTest.Areas.Admin.Data.Model.Charts;
 using WebOnlineAptitudeTest.Helper;
 using WebOnlineAptitudeTest.Models.Infrastructure;
+using WebOnlineAptitudeTest.Models.Repositories.Interface;
 
 namespace WebOnlineAptitudeTest.Areas.Admin.Controllers
 {
     public class HomesController : BaseController
     {
         private readonly IUnitOfWork _unitOfWork;
-        public HomesController(IUnitOfWork unitOfWork)
+        private readonly ICandidateRepository _candidateRepository;
+        private readonly IHistoryTestRepository _historyTestRepository;
+        public HomesController(ICandidateRepository candidateRepository,
+            IHistoryTestRepository historyTestRepository,
+            IUnitOfWork unitOfWork)
         {
+            _candidateRepository = candidateRepository;
+            _historyTestRepository = historyTestRepository;
             _unitOfWork = unitOfWork;
         }
 
@@ -38,6 +45,79 @@ namespace WebOnlineAptitudeTest.Areas.Admin.Controllers
             }, JsonRequestBehavior.AllowGet);
             json.MaxJsonLength = Int32.MaxValue;
             return json;
+        }
+
+        [HttpGet]
+        public JsonResult LoadCandiLast5Mounth()
+        {
+            var countCadinate = this._candidateRepository.GetAll()
+                .Where(x => x.Deleted == false && DateTime.Now.AddMonths(-4) <= x.CreatedDate)
+                .Select(y => new
+                {
+                    id = y.Id,
+                    CreatedMouth = y.CreatedDate.Month
+                })
+                .OrderBy(z => z.CreatedMouth)
+                .ToList();
+
+            var countPass = this._historyTestRepository.GetAll()
+                .Where(c => c.Candidate.Deleted == false && DateTime.Now.AddMonths(-4) <= c.TestSchedule.DateEnd)
+                .GroupBy(a => a.CandidateId)
+                .Select(x => new
+                {
+                    DateStart = x.FirstOrDefault().TestSchedule.DateStart,
+                    DateEnd = x.FirstOrDefault().TestSchedule.DateEnd,
+                    Mark = x.Sum(b => b.PercentMark) / 3
+                })
+                .ToList();
+
+            DateTime todayDate = DateTime.Now;
+
+            var thisMouth = countPass.Where(x => x.DateEnd.Month == todayDate.Month).ToList();
+            var before1Month = countPass.Where(x => x.DateEnd.Month == todayDate.AddMonths(-1).Month).ToList();
+            var before2Month = countPass.Where(x => x.DateEnd.Month == todayDate.AddMonths(-2).Month).ToList();
+            var before3Month = countPass.Where(x => x.DateEnd.Month == todayDate.AddMonths(-3).Month).ToList();
+            var before4Month = countPass.Where(x => x.DateEnd.Month == todayDate.AddMonths(-4).Month).ToList();
+
+            var result = new
+            {
+                SuccessThisMouth = thisMouth.Where(x => x.Mark >= 80).Count(),
+                FailedThisMouth = thisMouth.Where(x => x.Mark <= 80).Count(),
+                Successbefore1Month = before1Month.Where(x => x.Mark >= 80).Count(),
+                Failedbefore1Month = before1Month.Where(x => x.Mark < 80).Count(),
+                Successbefore2Month = before2Month.Where(x => x.Mark >= 80).Count(),
+                Failedbefore2Month = before2Month.Where(x => x.Mark < 80).Count(),
+                Successbefore3Month = before3Month.Where(x => x.Mark >= 80).Count(),
+                Failedbefore3Month = before3Month.Where(x => x.Mark < 80).Count(),
+                Successbefore4Month = before4Month.Where(x => x.Mark >= 80).Count(),
+                Failedbefore4Month = before4Month.Where(x => x.Mark < 80).Count(),
+
+                CountSuccess = countPass.Where(x => x.Mark >= 80).Count(),
+                CountFailed = countPass.Where(x => x.Mark < 80).Count()
+            };
+
+            var resultAll = new
+            {
+                ThisMouth = thisMouth.Where(x => x.Mark >= 80).Count(),
+                Before1Month = before1Month.Count(),
+                Before2Month = before2Month.Count(),
+                Before3Month = before3Month.Count(),
+                Before4Month = before4Month.Count(),
+
+                Count = countPass.Where(x => x.Mark >= 80).Count()
+            };
+
+            var json = Json(new
+            {
+                data = result
+            }, JsonRequestBehavior.AllowGet);
+            json.MaxJsonLength = Int32.MaxValue;
+            return json;
+        }
+        private int getPreviousMount(int last)
+        {
+            var thisMouth = DateTime.Now;
+            return thisMouth.AddMonths(-last).Month;
         }
     }
 }
